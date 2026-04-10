4ae15110474ecc330ba8874a45256ebb79c5b785
Joe Kim <joesm.genie@gmail.com>
2026-04-10 13:31:47 +0700

    feat: implement step load vault

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-use passwm::error::Result;
+use std::path::PathBuf;
+
+use clap::Parser;
+use passwm::{
+    cli::{Cli, Commands},
+    error::Result,
+    helper::prompt_password,
+    storage::{load_vault, vault_exists},
+    vault::Vault,
+};
 
 fn main() {
     if let Err(e) = run() {
@@ -8,5 +17,17 @@ fn main() {
 }
 
 fn run() -> Result<()> {
+    let cli = Cli::parse();
+    let vault_path = PathBuf::from(&cli.vault_path);
+
+    let master_password = prompt_password("🔑 Enter maste password: ")?;
+
+    let mut vault = if vault_exists(&vault_path) {
+        load_vault(&vault_path, &master_password)?
+    } else {
+        println!("📦 No vault found. Creating a new vault...");
+        Vault::new()
+    };
+
     Ok(())
 }
