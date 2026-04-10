f0b15ca135e94ca2f236ab80f1079bc72b34ba0b
Joe Kim <joesm.genie@gmail.com>
2026-04-10 13:54:36 +0700

    feat: implement step command

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, process::Command};
 
 use clap::Parser;
 use passwm::{
     cli::{Cli, Commands},
+    cmd::{cmd_add, cmd_delete, cmd_get, cmd_list, cmd_search, cmd_update},
     error::Result,
     helper::prompt_password,
-    storage::{load_vault, vault_exists},
+    storage::{load_vault, save_vault, vault_exists},
     vault::Vault,
 };
 
@@ -28,6 +29,29 @@ fn run() -> Result<()> {
         println!("📦 No vault found. Creating a new vault...");
         Vault::new()
     };
+
+    match cli.command {
+        Commands::Add {
+            service,
+            username,
+            password,
+        } => {
+            cmd_add(&mut vault, service, username, password)?;
+        }
+        Commands::List => cmd_list(&vault),
+        Commands::Get { service } => cmd_get(&vault, &service)?,
+        Commands::Update {
+            service,
+            username,
+            password,
+        } => {
+            cmd_update(&mut vault, &service, username, password)?;
+        }
+        Commands::Delete { service } => cmd_delete(&mut vault, &service)?,
+        Commands::Search { query } => cmd_search(&mut vault, &query),
+    }
+
+    save_vault(&vault, &vault_path, &master_password)?;
 
     Ok(())
 }
